5dfdb32a2f8794ed9f5fd43c5147cf84c7da2f65
ArkStyx <regis.petit@gmail.com>
2022-10-31 17:46:09 +0100

    Ajout de mes modifications pour l'exercice N°04 de la formation Spring 5 Guru

--- src/main/java/guru/springframework/sfgdependencyinjection/SfgDependencyInjectionApplication.java
+++ src/main/java/guru/springframework/sfgdependencyinjection/SfgDependencyInjectionApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
 import guru.springframework.sfgdependencyinjection.controllers.MyController;
+import guru.springframework.sfgdependencyinjection.controllers.pets.PetController;
 import guru.springframework.sfgdependencyinjection.controllers.profiles.I18nController;
 import guru.springframework.sfgdependencyinjection.controllers.types.ConstructorInjectedController;
 import guru.springframework.sfgdependencyinjection.controllers.types.PropertyInjectedController;
@@ -16,6 +17,10 @@ public class SfgDependencyInjectionApplication {
 	public static void main(String[] args) {
 		
 		ApplicationContext ctx = SpringApplication.run(SfgDependencyInjectionApplication.class, args);
+		
+		PetController petController = ctx.getBean("petController", PetController.class);
+		System.out.println("----- The Best Pet is ");
+		System.out.println(petController.whichPetIsTheBest());
 		
 		System.out.println("----- Profile Controller");
 		I18nController i18nController = (I18nController) ctx.getBean("i18nController");
